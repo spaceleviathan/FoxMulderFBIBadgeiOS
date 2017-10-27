256037fdfdbab7fc3d8c4dd38480918d8893895e
Sam M <sam.marlow@icloud.com>
2017-10-27 13:06:54 -0400

    Initial Commit

---
+++ Muller Badge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muller Badge
+//
+//  Created by Sam on 2017-10-27.
+//  Copyright © 2017 leviathanindustries. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
